705917c49cac556a279f69d7977f6af61473d9b9
tomek <tomek.wojnowski@o2.pl>
2015-03-09 20:39:36 +0100

    Jquery added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-alert("ok");
+$(function(){
+  $.ajax({
+    url: "/jsons/test.json",
+    type: "GET",
+    dataType: "json",
+    success: function(data){
+      console.log(data);
+    }
+  });
+});
