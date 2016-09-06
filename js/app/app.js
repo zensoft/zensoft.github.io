6ec27fc87822d680d8e6b8aeb4c3b7947a3cf0ba
zensoft <tomek.wojnowski@o2.pl>
2016-09-06 17:57:14 +0200

    commit

--- js/app/app.js
+++ js/app/app.js
@@ -2,7 +2,6 @@ define(['jquery','app/printer','app/calc'],function ($,Printer,Calc) {
 
     var calc = new Calc();
     var printer = new Printer();
-    printer.print("hello there");
     $(function(){
       //$("body").append("<h2>Hello html one more time</h2>");
       //$("body").append("<h2>" + myVal + "</h2>");
