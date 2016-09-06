0e57ff2a531505f7a639246464ac80499ecf832b
zensoft <tomek.wojnowski@o2.pl>
2016-09-06 17:55:44 +0200

    commit

--- js/app/app.js
+++ js/app/app.js
@@ -7,6 +7,7 @@ define(['jquery','app/printer','app/calc'],function ($,Printer,Calc) {
       //$("body").append("<h2>Hello html one more time</h2>");
       //$("body").append("<h2>" + myVal + "</h2>");
       //$("body").append("2 + 2 = " + calc.add(2,2));
+      $("body").trigger("my_test_event");
     });
 
 });
