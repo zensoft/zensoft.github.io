30cf0dcbe6a5e610391b7261a3b03097116627ed
Tomek <tomek.wojnowski@o2.pl>
2016-10-27 12:13:16 +0200

    Test dla Tomka 3

--- js/app/app.js
+++ js/app/app.js
@@ -2,16 +2,3 @@ function getBreakpoint() {
 	return;
 }
 
-
-define(['jquery','app/printer','app/calc'],function ($,Printer,Calc) {
-
-    var calc = new Calc();
-    var printer = new Printer();
-    $(function(){
-      //$("body").append("<h2>Hello html one more time</h2>");
-      //$("body").append("<h2>" + myVal + "</h2>");
-      //$("body").append("2 + 2 = " + calc.add(2,2));
-      $("body").trigger("my_test_event");
-    });
-
-});
