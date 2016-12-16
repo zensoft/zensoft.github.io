a19a6391d75a961d68646672194317e9c7d6de4f
Tomek <tomek.wojnowski@o2.pl>
2016-12-16 14:42:57 +0100

    ecomerce js tracking

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 console.log("ok");
+
+$(".promos").click(function(){
+	var d = $(this).data("promo");
+	dataLayer.push({
+    'event': 'promotionClick',
+    'ecommerce': {
+      'promoClick': {
+        'promotions': [
+         {
+           'id': d,                         
+           'name': d,
+           'creative': 'nie bardzo',
+           'position': '-1'
+         }]
+      }
+    },
+    'eventCallback': function() {
+      console.log("saved promo " + d);
+    }
+  });
+	
+	return true;
+});
