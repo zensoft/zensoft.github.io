e496d868065cdcc6be0351a72de6ccd347f39826
zensoft <tomek.wojnowski@o2.pl>
2017-07-30 15:40:40 +0200

    prace gtm

--- js/main.js
+++ js/main.js
@@ -21,3 +21,31 @@ $("ul").on("click",function(){
 
 
 });
+
+
+dataLayer.push({
+  'ecommerce': {
+    'currencyCode': 'EUR',                       // Local currency is optional.
+    'impressions': [
+     {
+       'name': 'Triblend Android T-Shirt',       // Name or ID is required.
+       'id': '12345',
+       'price': '15.25',
+       'brand': 'Google',
+       'category': 'Apparel',
+       'variant': 'Gray',
+       'list': 'Search Results',
+       'position': 1
+     },
+     {
+       'name': 'Donut Friday Scented T-Shirt',
+       'id': '67890',
+       'price': '33.75',
+       'brand': 'Google',
+       'category': 'Apparel',
+       'variant': 'Black',
+       'list': 'Search Results',
+       'position': 2
+     }]
+  }
+});
